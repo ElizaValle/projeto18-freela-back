ea9e1e7b2e5ea74ae5d7f557f64d6ee5ead415c6
ElizaValle <beteduvalle@gmail.com>
2023-10-05 09:48:48 -0300

    requisito para o cadastro - post signup

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,5 +1,25 @@
+import { db } from "../database/database.connection.js"
+import bcrypt from "bcrypt"
+
 export async function signUp(req, res) {
-    res.send("signup")
+    const { name, phone, states, city, email, password } = req.body
+
+    try {
+        const user = await db.query(`SELECT * FROM users WHERE email=$1;`,  [email])
+        if (user.rowCount > 0) return res.status(409).send({ message: "Email já cadastrado!" })
+
+        const passwordHash = bcrypt.hashSync(password, 10)
+
+        await db.query(`
+            INSERT INTO users (name, phone, states, city, email, password)
+                VALUES ($1, $2, $3, $4, $5, $6);
+        `, [name, phone, states, city, email, passwordHash])
+
+        res.sendStatus(201)
+
+    } catch (err) {
+        res.status(500).send(err.message)
+    } 
 }
 
 export async function signIn(req, res) {
--- src/schemas/user.schemas.js
+++ src/schemas/user.schemas.js
@@ -3,7 +3,7 @@ import joi from "joi"
 export const userSchema = joi.object({
     name: joi.string().trim().required(),
     phone: joi.string().regex(/^\d{11}$/).required(),
-    state: joi.string().required(),
+    states: joi.string().required(),
     city: joi.string().required(),
     email: joi.string().email().required(),
     password: joi.string().min(3).required(),
